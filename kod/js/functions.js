ebdffdd7ecb687d0f2e45189b85e5ad0a5ce4dab
elin57 <112911997+elin57@users.noreply.github.com>
2023-01-08 20:48:15 +0100

    Tog bort de tre gamla funktionerna som jag abstraherade

--- kod/js/functions.js
+++ kod/js/functions.js
@@ -212,44 +212,6 @@ function create_areas_filter() {
   array_each(LANGUAGES, create_area);
 }
 
-
-/*function create_levels_filter () {
-  function create_level (level) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#level_filter > ul"),
-      class: "selected",
-      textContent: level.name,
-    });
-    dom.dataset.id = level.id;
-  }
-  array_each(LEVELS, create_level);
-}
-// Create Subjects Filter
-function create_subjects_filter () {
-  function create_subject (subject) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#subject_filter > ul"),
-      class: "selected",
-      textContent: subject.name,
-    });
-    dom.dataset.id = subject.id;
-  }
-  array_each(SUBJECTS, create_subject);
-}
-// Create Search Field
-function create_language_filter () {
-  function create_element (data) {
-    const dom = create_filter_element({
-      parent: document.querySelector("#language_filter > ul"),
-      class: "selected",
-      textContent: data.name,
-    });
-    dom.dataset.id = data.id;
-  }
-  array_each(LANGUAGES, create_element);
-}*/
-
-
 // G / VG (see details in specification)
 // CODE according to specifications
 
